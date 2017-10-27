13d6a72d7ba85ed56c2c530f6e662ea9f396cd22
Ally <allyrvargas@gmail.com>
2017-10-27 16:18:11 -0700

    Menu added

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,24 +1,25 @@
 require_relative 'entry'
 
-	class AddressBook
-		attr_reader :entries
+class AddressBook
+	attr_reader :entries
 
-		def initialize
-			@entries = []
-		end
+  	def initialize
+    	@entries = []
+  	end
+
+  	def add_entry(name, phone_number, email)
+    	index = 0
+    	entries.each do |entry|
+      	if name < entry.name
+        	break
+      	end
+      	index+= 1
+    end
+    	entries.insert(index, Entry.new(name, phone_number, email))
 end
 
-	 def add_entry(name, phone_number, email)
-     #create a variable to store the insertion 'index'
-     index = 0
-     entries.each do |entry|
- 
-       if name < entry.name
-       	break
-       end
-       index+= 1
-     end
-     #insert new entry
-     entries.insert(index, Entry.new(name, phone_number, email))
-   end
+  	def remove_entry(name, phone_number, email)
+    	target_index = entries.find_index { |entry| entry.name == name && entry.phone_number == phone_number && entry.email == email }
+    	entries.delete_at(target_index)
+  	end
 end
